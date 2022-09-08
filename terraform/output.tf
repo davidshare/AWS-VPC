259cbe1a9060359a62352f83c582c35215aa1467
David Essien <davidessienshare@gmail.com>
2022-09-08 05:36:41 +0100

    update the vpc output to show the vpc name

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,15 @@
+# output "vpc_id" {
+#   value = values(aws_vpc.vpcs).*.id
+# }
+
 output "vpc_id" {
-  value = values(aws_vpc.vpcs).*.id
+  value = {
+    for vpc_name, vpc in aws_vpc.vpcs : vpc_name => vpc.id
+  }
 }
+
+# output "vpc_id" {
+#   value = {
+#     for k, bd in mso_schema_template_bd.bd : k => bd.name
+#   }
+# }
